026b3f273d5b0e9b37b4c4469734f12493021710
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-10 20:32:43 -0400

    added classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,50 @@
 class Tooltip {}
 
-class Projectitem {}
+class Projectitem {
+    constructor(id) {
+        this.id = id;
+        this.connectMoreInfoButton();
+        this.connectSwithchButton();
+    }
 
-class ProjectList {}
+    connectMoreInfoButton(){
 
-class App {}
+    }
+
+    connectSwithchButton() {
+        const projectItemElement = document.getElementById(this.id);
+        const switchBtn = projectItemElement.querySelector('button:last-of-type');
+        // switchBtn.addEventListener('click', switchProject)
+    }
+}
+
+class ProjectList {
+    projects = []
+   
+    constructor(type) {
+        const prjItems = document.querySelectorAll(`#${type}-projects li`);
+        for (const prjItem of prjItems) {
+            this.projects.push(new Projectitem(prjItem.id))   
+        }
+        console.log(this.projects)
+    }
+
+    addProject() {
+
+    }
+
+    switchProject(projectId) {
+        const projectIndex = this.projects.findIndex(p => p.id === projectId)
+        // this.projects.splice(projectIndex, 1);
+        this.projects = this.projects.filter(p = p.id !== projectId);
+    }
+}
+
+class App {
+    static init() {
+        const activeProjectList = new ProjectList('active');
+        const finishedProjectList = new ProjectList('finished');
+    }
+}
+
+App.init();
