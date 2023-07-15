6783326286047977c39b9ee65d7d8855ee8dcbed
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-15 11:58:35 -0400

    added documentation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,33 +13,33 @@ class DOMHelper { // called in connectSwithchButton() in ProjectItem
 }
 
 class Tooltip {
-    constructor(closeNotifierFunction) { //function parameter received on instatiation in ProjectItem showMoreInfoHandler(). Ensures that a toolTtip is not active.
+    constructor(closeNotifierFunction) { //function parameter received on instatiation in ProjectItem showMoreInfoHandler(). Ensures that a toolTtip dialog box status is not active.
         this.closeNotifier = closeNotifierFunction;
     }
 
-    closeTooltip = () => {// when using arrow functions this refers to the class automatically
+    closeTooltip = () => {// when using arrow functions this refers to the class automatically on function being called. no need to bind call to this
         this.detach(); // removes toolTip
-        this.closeNotifier() //received from constructor. sets hasactiveToolTip to false
+        this.closeNotifier() //calls method passed to constructor on instatiation that sets hasActiveToolTip value to false when tooltip is closed allowing another toolTip dialog to be displayed if neccessary
     }
 
     detach(){ 
-        this.element.remove()
-        // this.element.parentElement.removeChild(this.element)
+        this.element.remove() // refers to the element stored in attach method
+        // this.element.parentElement.removeChild(this.element) //older browser support
     }
 
-    attach() {
-        const tooltipElement = document.createElement('div')
+    attach() { // called from showMoreInfoHandler method in ProjectItem
+        const tooltipElement = document.createElement('div') // div for tooltip element
         tooltipElement.className = 'card';
         tooltipElement.textContent = 'DUMDUM DUMMY' 
-        tooltipElement.addEventListener('click', this.closeTooltip) //calls closeToolTip to remove the toolTip and set hascActive to false
-        this.element = tooltipElement // enables the tooltip to be removed on click br refering the element to the closeToolTip method
+        tooltipElement.addEventListener('click', this.closeTooltip) // adds eventListener to div and calls closeToolTip on click to remove the toolTip and set hascActive to false. binding is not needed to to close tool tip being an arrow function 
+        this.element = tooltipElement // makes the div accesable in the detach method
         document.body.append(tooltipElement)
 
     }
 }
 
 class Projectitem {
-    hasactiveToolTip = false; // set up to see if item already has an active toolTip
+    hasActiveToolTip = false; // set up to see if item already has an active toolTip
 
     constructor(id, updateProjectListFunction, type) { // parameters receivved from ProjectList constructor on ProjectItem instatiation
         this.id = id;
@@ -49,20 +49,20 @@ class Projectitem {
     }
 
     showMoreInfoHandler() {
-        if (this.hasactiveToolTip) {
+        if (this.hasActiveToolTip) { //stops addition toolTip project information dialog from being added if it is already present
             return;
         }
-        const tooltip = new Tooltip(() => { // called on instantiation to ensure that a toolTip is able to be aded
-            this.hasactiveToolTip = false;
+        const tooltip = new Tooltip(() => { //creates toolTip object. On instantiation passes anonomous function to the constructor that when called sets the value of hasActiveTooltip to false when toolTip is closed
+            this.hasActiveToolTip = false;
         })
-        tooltip.attach(); // makes sure that clicking won't add another toolTip
-        this.hasactiveToolTip = true;
+        tooltip.attach();  //calls attatch method in Tooltip object to create and display toolTip element
+        this.hasActiveToolTip = true; // sets tooltip element status to active to ensure that that clicking More Info Btn won't add another toolTip.
     }
    
-    connectMoreInfoButton(){
+    connectMoreInfoButton(){ //connects to more info Button
         const projectItemElement = document.getElementById(this.id);
         const moreInfoBtn = projectItemElement.querySelector('button:first-of-type')
-        moreInfoBtn.addEventListener('click', this.showMoreInfoHandler);
+        moreInfoBtn.addEventListener('click', this.showMoreInfoHandler); //adds eventListtener to More Info Btn. calls method whe clicked
     }
 
     connectSwithchButton(type) { //type received from call to functiom
@@ -73,7 +73,7 @@ class Projectitem {
         switchBtn.addEventListener('click', this.updateProjectListHandler.bind(null, this.id))//received from update. calls switchProject on click then switchHandler/addProject is invoked.
     }
 
-    update(updateProjectListFn, type) { //received on call from addProject
+    update(updateProjectListFn, type) { //received on call from addProject in ProjectList
         this.updateProjectListHandler = updateProjectListFn //received from constructor. tied to addProject/switchProject
         this.connectSwithchButton(type); ///passes type to connectSwithchButton
     }
@@ -98,10 +98,10 @@ class ProjectList {
     addProject(project) { // called in switchProject from switchHandler in the instance that will add project - gets passed projectId from swithcProject
         this.projects.push(project) //gets project from array
         DOMHelper.moveElement(project.id, `#${this.type}-projects ul`) //calls DOMHelper static method moveElemt and passes elementId and ul Id type of list item to be moved
-        project.update(this.switchProject.bind(this), this.type)//reaches out to project we get as argument calls update method. used as defined for new instance. type lets it know which list its in(important for updateing buttom txt)
+        project.update(this.switchProject.bind(this), this.type)//calls update method in ProjectItem and passes swithcProject method. reaches out to project we get as argument. used as defined in class App for newInstance. type lets it know which list its in(important for updateing buttom txt)
     }
 
-    switchProject(projectId) { //passed to ProjectItem constructor then to eventListener / geyrs projectTd from eventListener passes to addProject
+    switchProject(projectId) { //passed to ProjectItem constructor then to eventListener / getrs projectTd from eventListener passes to addProject
         // const projectIndex = this.projects.findIndex(p => p.id === projectId)
         // this.projects.splice(projectIndex, 1);
         this.switchHandler(this.projects.find(p => p.id === projectId)) // gets the project that we want to switch for addProject method to carry out. calls addProject from evenListner fireing switchProject
