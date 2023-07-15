bd34ae437ec53094e7f21e3ef06ad32834c36bc6
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-15 09:58:12 -0400

    added documentation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,26 +1,25 @@
 class DOMHelper { // called in connectSwithchButton() in ProjectItem
     static clearEventListeners(element) { //gets rid of old ebentListener for an element. called from connectSwithchButton
-        const clonedElement = element.cloneNode(true) // makes deep clone of button
-        element.replaceWith(clonedElement) // creates new button with a clone of old one clearing eventListener
+        const clonedElement = element.cloneNode(true) // makes deep clone of button. Default value is false.
+        element.replaceWith(clonedElement) // called in connectSwithchButton. Creates new button with a clone of old one clearing eventListener.
         return clonedElement; // returns the new button
     }
 
-    static moveElement(elementId, newDestinationSelector) { // parameters received from addProduct 
-        const element = document.getElementById(elementId);
-        const destinationElement = document.querySelector(newDestinationSelector);
-        destinationElement.append(element)
+    static moveElement(elementId, newDestinationSelector) { // parameters of project to add received from addProject
+        const element = document.getElementById(elementId); // id of element to be moved
+        const destinationElement = document.querySelector(newDestinationSelector); //place where elemt will be moved
+        destinationElement.append(element) // puts moved element in its place
     }
 }
 
 class Tooltip {
-    constructor(closeNotifierFunction) {
+    constructor(closeNotifierFunction) { //function parameter received on instatiation in ProjectItem showMoreInfoHandler(). Ensures that a toolTtip is not active.
         this.closeNotifier = closeNotifierFunction;
     }
 
     closeTooltip = () => {// when using arrow functions this refers to the class automatically
-        this.detach();
-        this.closeNotifier()
-
+        this.detach(); // removes toolTip
+        this.closeNotifier() //received from constructor. sets hasactiveToolTip to false
     }
 
     detach(){ 
@@ -32,8 +31,8 @@ class Tooltip {
         const tooltipElement = document.createElement('div')
         tooltipElement.className = 'card';
         tooltipElement.textContent = 'DUMDUM DUMMY' 
-        tooltipElement.addEventListener('click', this.closeTooltip)
-        this.element = tooltipElement
+        tooltipElement.addEventListener('click', this.closeTooltip) //calls closeToolTip to remove the toolTip and set hascActive to false
+        this.element = tooltipElement // enables the tooltip to be removed on click br refering the element to the closeToolTip method
         document.body.append(tooltipElement)
 
     }
@@ -53,10 +52,10 @@ class Projectitem {
         if (this.hasactiveToolTip) {
             return;
         }
-        const tooltip = new Tooltip(() => {
+        const tooltip = new Tooltip(() => { // called on instantiation to ensure that a toolTip is able to be aded
             this.hasactiveToolTip = false;
         })
-        tooltip.attach(); 
+        tooltip.attach(); // makes sure that clicking won't add another toolTip
         this.hasactiveToolTip = true;
     }
    
@@ -69,14 +68,14 @@ class Projectitem {
     connectSwithchButton(type) { //type received from call to functiom
         const projectItemElement = document.getElementById(this.id);
         let switchBtn = projectItemElement.querySelector('button:last-of-type');
-        switchBtn = DOMHelper.clearEventListeners(switchBtn) // passes button to function to clear any pre existing eventListener
-        switchBtn.textContent = type ==='active' ? 'Finish' : 'Activate';
-        switchBtn.addEventListener('click', this.updateProjectListHandler.bind(null, this.id))//calls switchProject on click then switchHandler/addProject is invoked
+        switchBtn = DOMHelper.clearEventListeners(switchBtn) // passes button to function to clear any pre-existing eventListeners
+        switchBtn.textContent = type ==='active' ? 'Finish' : 'Activate'; //changes the tect when active/finished status changes
+        switchBtn.addEventListener('click', this.updateProjectListHandler.bind(null, this.id))//received from update. calls switchProject on click then switchHandler/addProject is invoked.
     }
 
     update(updateProjectListFn, type) { //received on call from addProject
-        this.updateProjectListHandler = updateProjectListFn
-        this.connectSwithchButton(type); ///passes type to  connectSwithchButton
+        this.updateProjectListHandler = updateProjectListFn //received from constructor. tied to addProject/switchProject
+        this.connectSwithchButton(type); ///passes type to connectSwithchButton
     }
 }
 
